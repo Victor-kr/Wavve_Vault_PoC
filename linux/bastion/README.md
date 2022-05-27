675cff877581ba8f729ea396872749de49bd5942
Ubuntu <ubuntu@ip-172-31-44-220.ap-northeast-2.compute.internal>
2022-05-27 03:12:21 +0000

    udpate sources

--- linux/bastion/README.md
+++ linux/bastion/README.md
@@ -6,11 +6,5 @@ Bastion Host 에서 Internal Server 로 접근시는 Vault CA 를 사용한다.
 $ export VAULT_ADDR="http://172.31.44.220:8200"
 $ export VAULT_TOKEN="hvs.zpu3IwU6OyNBg7iDN8DbWb3K"
 $ chmod +x ./caClientManagement.sh
-$ ./caClientManagement.sh -s 172.31.43.32 -k myTestkey
-```
- 
-이제 bastion 호스트에서 web 서버로 접근시 key pair 를 이용하여 접속 가능하다.
-
-```console
-$ ssh -i ~/.ssh/myTestkey_cert.pub -i ~/.ssh/myTestkey ubuntu@172.31.43.32
+$ ./caClientManagement.sh -s 172.31.43.32 -u ubuntu
 ```
--- linux/bastion/caClientManagement.sh
+++ linux/bastion/caClientManagement.sh
@@ -55,15 +55,21 @@ function vault-approle-login() {
 function vault-sign-ssh-key() {
   path=$1
   shift
-  public_key_file="$*"
-  public_key=$(cat "${public_key_file}.pub")
+  ssh_user="$*"
+  shift 
+
+
+  key_file="${HOME}/.ssh/id_rsa_${ssh_user}"
+  public_key=$(cat ${key_file}.pub)
+  echo "PUBLIC KEY : $public_key"
 
   postfix=$(echo $RANDOM | md5sum | head -c 20; echo;)
   payload="/tmp/sign-ssh-key-${postfix}.json"
 
   jq -n \
     --arg public_key "$public_key" \
-    '{"public_key": $ARGS.named["public_key"]}' > "${payload}"
+    --arg valid_principals "$ssh_user" \
+    '{"public_key": $ARGS.named["public_key"],"valid_principals": $ARGS.named["valid_principals"]}' > "${payload}"
 
   res=$(curl \
     --silent \
@@ -74,22 +80,22 @@ function vault-sign-ssh-key() {
 
   sudo rm -rf  "${payload}"
    
-  echo $res > "${public_key_file}_cert.pub"
+  echo $res | sudo tee "${key_file}_cert.pub"
 
-  sudo chmod 400 "${public_key_file}"
-  sudo chmod 400 "${public_key_file}.pub"
-  sudo chmod 400 "${public_key_file}_cert.pub"
+  sudo chmod 600 "${key_file}"
+  sudo chmod 644 "${key_file}.pub"
+  sudo chmod 644 "${key_file}_cert.pub"
  
 }
 
 #---------------------------------------------------------------
 #  Getting input parameters
 #---------------------------------------------------------------
-while getopts s:k: flag
+while getopts s:u: flag
 do
     case "${flag}" in
         s) server=${OPTARG};;
-        k) keyname=${OPTARG};; 
+        u) ssh_user=${OPTARG};; 
     esac
 done
 
@@ -98,6 +104,11 @@ if [ -z "$sshd_config_file" ]; then
   sshd_config_file="/etc/ssh/sshd_config"
 else 
    echo '[Info] Config ${sshd_config_file}..'
+fi
+
+if [ -z "$ssh_user" ]; then 
+  echo '[Error] Please put a user name to create key'
+  exit 1
 fi
 
 #---------------------------------------------------------------
@@ -127,8 +138,10 @@ export VAULT_ADDR=${VAULT_ADDR:-http://172.31.44.220:8200}
 export APP_TOKEN=$(vault-approle-login "carole")
 export VAULT_TOKEN=$APP_TOKEN
 
-public_key_file="${HOME}/.ssh/${keyname}"
-ssh-keyscan "${server}" >> known_hosts
-ssh-keygen -t rsa -P "" -f "$public_key_file"
+key_file="${HOME}/.ssh/id_rsa_${ssh_user}"
 
-vault-sign-ssh-key "ssh-client-signer/sign/ssh-ca-role" "$public_key_file"
+ssh-keygen -t rsa-sha2-256 -N "" -C "${ssh_user}" -f "${key_file}"
+
+vault-sign-ssh-key "ssh-client-signer/sign/ssh-ca-role" "${ssh_user}"
+
+echo "Trying.... ssh -i .ssh/id_rsa_${ssh_user}.signed.pub -i .ssh/id_rsa_${ssh_user} ${ssh_user}@${server}"
