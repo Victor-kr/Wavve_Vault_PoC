2a48e5f3b2029094c275f5a1affbe732b38f9bb9
kin3303 <48729027+kin3303@users.noreply.github.com>
2022-06-13 18:51:10 +0900

    Update caServerManagement.sh

--- linux/internal/caServerManagement.sh
+++ linux/internal/caServerManagement.sh
@@ -3,15 +3,6 @@
 #---------------------------------------------------------------
 # Functions
 #---------------------------------------------------------------
-function curlWapper() {
-  url="${@: -1}"
-  if [[ $url = https://* ]]; then
-    curl -k $*
-  else
-    curl $*
-  fi
-}
-
 function backup-sshd-config(){
   file="$1"
   if [ -f ${file} ]
@@ -77,7 +68,7 @@ done
 export VAULT_ADDR=${VAULT_ADDR:-http://172.31.44.220:8200} 
 
 sudo rm -rf /etc/ssh/trusted-user-ca-keys.pem
-curlWapper -o ${HOME}/trusted-user-ca-keys.pem ${VAULT_ADDR}/v1/ssh-client-signer/public_key
+curl -k -o ${HOME}/trusted-user-ca-keys.pem ${VAULT_ADDR}/v1/ssh-client-signer/public_key
 sudo mv ${HOME}/trusted-user-ca-keys.pem /etc/ssh/trusted-user-ca-keys.pem
 
 backup-sshd-config "${sshd_config_file}"
