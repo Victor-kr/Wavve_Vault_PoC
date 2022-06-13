51f626b12a4718151eafe506e8e759406655e83a
kin3303 <48729027+kin3303@users.noreply.github.com>
2022-06-13 15:33:50 +0900

    Update renewPassServer.ps1

--- windows/server/renewPassServer.ps1
+++ windows/server/renewPassServer.ps1
@@ -1,5 +1,20 @@
 Clear-Host
 
+# Ignore SSL 
+add-type @"
+   using System.Net;
+   using System.Security.Cryptography.X509Certificates;
+   public class TrustAllCertsPolicy : ICertificatePolicy {
+      public bool CheckValidationResult(
+      ServicePoint srvPoint, X509Certificate certificate,
+      WebRequest request, int certificateProblem) {
+      return true;
+   }
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+
 $ErrorActionPreference = "Continue"  
 $VerbosePreference = 'Continue' 
 
